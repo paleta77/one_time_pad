a8b3e2f4ea3db90411bf73d0cf7caa68c2902f54
Paweł Szrubas <pawel050673@gmail.com>
2018-10-29 21:02:12 +0100

    skonczylem na wyznaczaniu x

--- src/one_time_pad/One_time_pad.java
+++ src/one_time_pad/One_time_pad.java
@@ -5,6 +5,10 @@
  */
 package one_time_pad;
 
+import java.util.Arrays;
+
+
+
 /**
  *
  * @author pawel
@@ -14,8 +18,38 @@ public class One_time_pad {
     /**
      * @param args the command line arguments
      */
+    public static boolean relatywniePierwsza(int a, double b)
+    {
+        boolean relatywniePierwsza = false;
+        
+        while (a != b){
+            if (a > b) a -= b;               
+            else b -= a;              
+            }
+        
+        if (a==1) return true;
+        else return false;
+    }
+    
+    public static boolean[] blum_blum_shub(int p, int q, int dlugosc){
+        boolean pad[] = new boolean[dlugosc]; //klucz
+        int m = p*q;
+        int x;
+        
+        for(x=m;;x--) //x ma byc releatywnie pierwsze do m
+        {
+            if (relatywniePierwsza(x,m)==true) break;
+        }
+        
+        System.out.print(x);
+        
+        return pad;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        blum_blum_shub(20,2,5);
+        //System.out.print(relatywniePierwsza(20,4));
     }
     
 }
