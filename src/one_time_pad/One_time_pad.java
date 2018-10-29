4805fd3ebfa856017963de34942a66258a3b9d72
Paweł Szrubas <pawel050673@gmail.com>
2018-10-29 19:06:03 +0100

    Utworzenie projektu

---
+++ src/one_time_pad/One_time_pad.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package one_time_pad;
+
+/**
+ *
+ * @author pawel
+ */
+public class One_time_pad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
