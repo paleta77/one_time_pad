b019dc28284d0c54dc9468c21d37ca27cdecb0d6
agnieszka2018 <baga16@o2.pl>
2018-11-06 20:21:23 +0100

    zapis i odczyt z pliku

--- src/one_time_pad/One_time_pad.java
+++ src/one_time_pad/One_time_pad.java
@@ -5,9 +5,16 @@
  */
 package one_time_pad;
 
+//do szyfrowania
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+//do wczytywania i zapisu
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  *
@@ -17,9 +24,8 @@ public class One_time_pad {
 
     /**
      * @param args the command line arguments
-     * 
+     *
      */
-    
     public static List generujKluczSzyfrujacy(int dlugosc) {
         Random generator = new Random(System.currentTimeMillis()); //Inicjalizacja generatora ktory za ziarno pobiera aktualny czas w milisekunach
         List los = new ArrayList(); //Lista tablicowa. Szybszy odczyt niz dla listy linkowanej
@@ -28,9 +34,45 @@ public class One_time_pad {
         }
         return los;
     }
-    
+
     public static void main(String[] args) {
+        String plik_1 = "plik.pdf";                //dowolna ścieżka do pliku
+
+        FileInputStream daneDoSzyfrowania = null;
+        FileOutputStream zapisDoPliku = null;
+
+        //wczytywanie i zapis
+        try {
+            daneDoSzyfrowania = new FileInputStream(plik_1);
+            zapisDoPliku = new FileOutputStream("plik_2.pdf");
+        } catch (FileNotFoundException wyjatek) {
+            System.out.println("Nie znaleziono takiego pliku!");
+        }
+
+        try {
+            byte[] bufor = new byte[1024];          //tablica typu byte
+            int i = 0;                              //iterator
+            while (daneDoSzyfrowania.read(bufor) != -1) {
+                //szyfrowanie ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                zapisDoPliku.write(bufor);
+                ++i;
+            }
+        } catch (IOException wyjatek) {
+            System.out.println("Błąd wejścia-wyjścia!");
+        }
+
+        try {
+            if (daneDoSzyfrowania != null) {
+                daneDoSzyfrowania.close();
+            }
+            if (zapisDoPliku != null) {
+                zapisDoPliku.close();
+            }
+        } catch (IOException wyjatek) {
+            System.out.println("Błąd zamykania strumieni!");
+        }
+
+        //szyfrowanie
         System.out.print(generujKluczSzyfrujacy(20));
     }
-    
 }
