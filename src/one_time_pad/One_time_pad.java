6c89127fa2a160d2a840694c1c6897bbaa0745de
Paweł Szrubas <pawel050673@gmail.com>
2018-11-06 18:40:39 +0100

    Dodano losowanie

--- src/one_time_pad/One_time_pad.java
+++ src/one_time_pad/One_time_pad.java
@@ -5,6 +5,10 @@
  */
 package one_time_pad;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author pawel
@@ -13,9 +17,20 @@ public class One_time_pad {
 
     /**
      * @param args the command line arguments
+     * 
      */
+    
+    public static List generujKluczSzyfrujacy(int dlugosc) {
+        Random generator = new Random(System.currentTimeMillis()); //Inicjalizacja generatora ktory za ziarno pobiera aktualny czas w milisekunach
+        List los = new ArrayList(); //Lista tablicowa. Szybszy odczyt niz dla listy linkowanej
+        for (int i = 0; i < dlugosc; i++) {
+            los.add(generator.nextBoolean()); //Dodawanie do listy bit po bicie 
+        }
+        return los;
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print(generujKluczSzyfrujacy(20));
     }
     
 }
