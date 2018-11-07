85515e5b39ab165f360589d0707995b77781e693
agnieszka2018 <baga16@o2.pl>
2018-11-07 10:53:10 +0100

    zapis i odczyt

--- src/one_time_pad/One_time_pad.java
+++ src/one_time_pad/One_time_pad.java
@@ -5,6 +5,17 @@
  */
 package one_time_pad;
 
+//do szyfrowania
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+//do wczytywania i zapisu
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  *
  * @author pawel
@@ -13,9 +24,61 @@ public class One_time_pad {
 
     /**
      * @param args the command line arguments
+     *
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static List generujKluczSzyfrujacy(int dlugosc) {
+        Random generator = new Random(System.currentTimeMillis()); //Inicjalizacja generatora ktory za ziarno pobiera aktualny czas w milisekunach
+        List los = new ArrayList(); //Lista tablicowa. Szybszy odczyt niz dla listy linkowanej
+        for (int i = 0; i < dlugosc; i++) {
+            los.add(generator.nextBoolean()); //Dodawanie do listy bit po bicie 
+        }
+        return los;
     }
-    
+
+    public static void zapis_i_odczyt_plik(String plik_1) {
+
+        FileInputStream daneDoSzyfrowania = null;
+        FileOutputStream zapisDoPliku = null;
+
+        //wczytywanie i zapis
+        try {
+            daneDoSzyfrowania = new FileInputStream(plik_1);
+            zapisDoPliku = new FileOutputStream("plik_2.pdf");
+        } catch (FileNotFoundException wyjatek) {
+            System.out.println("Nie znaleziono takiego pliku!");
+        }
+
+        try {
+            byte[] bufor = new byte[1024];          //tablica typu byte
+            int i = 0;                              //iterator
+            while (daneDoSzyfrowania.read(bufor) != -1) {
+                //zamiana na bity i szyfrowanie ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                zapisDoPliku.write(bufor);
+                ++i;
+            }
+
+            //System.out.println(bufor);
+        } catch (IOException wyjatek) {
+            System.out.println("Błąd wejścia-wyjścia!");
+        }
+
+        try {
+            if (daneDoSzyfrowania != null) {
+                daneDoSzyfrowania.close();
+            }
+            if (zapisDoPliku != null) {
+                zapisDoPliku.close();
+            }
+        } catch (IOException wyjatek) {
+            System.out.println("Błąd zamykania strumieni!");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String plik_1 = "plik.pdf";                //dowolna ścieżka do pliku
+        zapis_i_odczyt_plik(plik_1);
+        //szyfrowanie
+        //System.out.print(generujKluczSzyfrujacy(20));
+    }
 }
